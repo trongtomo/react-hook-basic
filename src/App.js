8bae0d608ca08987c136272dc95ac0b7722d8bff
trongtomo <binguyenquoctrong@gmail.com>
2022-03-13 20:46:06 +0700

    #4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 const App = () => {
+  let name = "trong";
+  let number = 69;
+  let obj = { name: "Trong", id: "123" };
+  let link = "https://duckduckgo.com/";
   return (
     <div className="App">
+      {/* {console.log("Obj:", obj)} */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React</h1>
+        <h1>
+          Hello {name} - {number}
+          <br />
+          <a href={link} target="_blank">
+            Use duckduckgo
+          </a>
+        </h1>
+        <p>{JSON.stringify(obj)}</p>
       </header>
     </div>
   );
