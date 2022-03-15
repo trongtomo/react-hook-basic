86a67e38e6e8f6f1e6f5bc55b3141b06adffa0e6
trongtomo <binguyenquoctrong@gmail.com>
2022-03-15 10:36:39 +0700

    #8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Navigation from "./views/Navigation";
+import {useState} from "react";
 const App = () => {
   const handleOnClick =() =>{
-    alert("Clicked!")
+    setName(address)
   }
-  let name = "trong";
+  const handleOnChangeInput =(event) =>{
+    setAddress(event.target.value)
+  }
+  //name : phan tu, setName: bien de su dung khi xu ly thay doi
+  let [name,setName]=useState("trong"); // [a1,b1, ...c1]
+  const [address,setAddress]=useState("");
   let number = 69;
   let obj = { name: "Trong", id: "123" };
   let link = "https://duckduckgo.com/";
+  //re-render
   return (
     <div className="App">
       <Navigation />
@@ -18,11 +25,12 @@ const App = () => {
         <h1>
           Hello {name} - {number}
           <br />
-          <a href={link} target="_blank">
+          <a href={link} target="_blank" rel="noreferrer" >
             Use duckduckgo
           </a>
         </h1>
-        <p>{JSON.stringify(obj)}</p>
+        {/* <p>{JSON.stringify(obj)}</p> */}
+        <input type="text" value={address} onChange={(event) =>handleOnChangeInput(event)}/>
         <button type="submit" onClick={(event)=>handleOnClick(event)}>Click me</button>
       </header>
     </div>
