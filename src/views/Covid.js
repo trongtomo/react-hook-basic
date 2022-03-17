2b46787723af7995dd79f83bdf913f333867ab9f
trongtomo <binguyenquoctrong@gmail.com>
2022-03-17 14:05:04 +0700

    #17

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -1,50 +1,56 @@
-
-import { useEffect,useState } from 'react';
-import axios from 'axios';
-import moment from 'moment';
- const Covid = () => {
-     const [dataCovid, setdataCovid]= useState([]);
-     //= componentDidMount()
-     useEffect(async()=>{
-        let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2022-03-13T00%3A00%3A00Z&to=2022-03-15T00%3A00%3A00Z')
-        let data=res&&res.data?res.data:[];
-        if (data&&data.length>0){
-            data.map(item=>{
-                item.Date=moment(item.Date).format('DD/MM/YYYY')
-                return item;
-            })
-        }
-        setdataCovid(data);
-        console.log('check res', res)
-     },[]);
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+const Covid = () => {
+  const [dataCovid, setdataCovid] = useState([]);
+  //= componentDidMount()
+  useEffect(() => {
+    async function fectchData() {
+      let res = await axios.get(
+        "https://api.covid19api.com/country/vietnam?from=2022-03-13T00%3A00%3A00Z&to=2022-03-17T00%3A00%3A00Z"
+      );
+      // res && res.data la dieu kien ? tra ve true : tra ve false
+      let data = res && res.data ? res.data : [];
+      if (data && data.length > 0) {
+        data.map((item) => {
+          item.Date = moment(item.Date).format("DD/MM/YYYY");
+          return item;
+        });
+        data = data.reverse();
+      }
+      setdataCovid(data);
+      console.log("check res", res);
+    }
+    fectchData();
+  }, []);
   return (
     <table id="customers">
-        {console.log("data Covid", dataCovid)}
-    <thead>
+      {console.log("data Covid", dataCovid)}
+      <thead>
         <tr>
-            <th>Date</th>
-        <th>Confirmed</th>
-      <th>Active</th>
-      <th>Deaths</th>
-      <th>Recovered</th>
+          <th>Date</th>
+          <th>Confirmed</th>
+          <th>Active</th>
+          <th>Deaths</th>
+          <th>Recovered</th>
         </tr>
-    </thead>
+      </thead>
       <tbody>
-          {dataCovid && dataCovid.length>0 &&
-          dataCovid.map(item=>{
-              return(
-                <tr key={item.ID}>
-                    <td>{item.Date}</td>
+        {dataCovid &&
+          dataCovid.length > 0 &&
+          dataCovid.map((item) => {
+            return (
+              <tr key={item.ID}>
+                <td>{item.Date}</td>
                 <td>{item.Confirmed}</td>
                 <td>{item.Active}</td>
                 <td>{item.Deaths}</td>
                 <td>{item.Recovered}</td>
               </tr>
-              )
+            );
           })}
-     
       </tbody>
-  </table>
-  )
-}
+    </table>
+  );
+};
 export default Covid;
