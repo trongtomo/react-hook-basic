853719363abf9b9cb11cfd9ce280324726aa2c0c
trongtomo <binguyenquoctrong@gmail.com>
2022-03-28 15:05:08 +0700

    #24

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,7 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 const DetailBlog = () => {
+  let history = useHistory();
+  const handleBackData = () => {
+    history.push("/blog");
+  };
   let { id } = useParams();
-  return <div>DetailBlog with id = {id}</div>;
+  return (
+    <>
+      <div>DetailBlog with id = {id}</div>
+      <div>
+        <span onClick={handleBackData}>BACK </span>{" "}
+      </div>
+    </>
+  );
 };
 
 export default DetailBlog;
