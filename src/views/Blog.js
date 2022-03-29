7d814f2cf0a1cbb16acb9f2b124c1197e03d9b0f
trongtomo <binguyenquoctrong@gmail.com>
2022-03-29 11:56:42 +0700

    #28

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,56 +1,55 @@
 import useFetch from "../customize/fetch";
 import "./Blog.scss";
 import { Link, useHistory } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import AddNewBlog from "./AddNewBlog";
 const Blog = () => {
+  const [newData, setNewData] = useState([]);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  let history = useHistory();
-  const handleAddNew = () => {
-    history.push("/addnewblog");
-  };
   const {
     data: dataBlogs,
     isLoading,
     isError,
   } = useFetch(`https://jsonplaceholder.typicode.com/posts`, false);
-  // console.log("check data", dataBlogs);
-  let newData = [];
-  if (dataBlogs && dataBlogs.length > 0) {
-    newData = dataBlogs.slice(0, 10);
-  }
+
+  useEffect(() => {
+    if (dataBlogs && dataBlogs.length > 0) {
+      let data = dataBlogs.slice(0, 9);
+      setNewData(data);
+    }
+  }, [dataBlogs]);
+
+  const handleAddNew = (blog) => {
+    let data = newData;
+    data.unshift(blog);
+    setShow(false);
+    setNewData(data);
+  };
+  const deletePost = (id) => {
+    let data = newData;
+    data = data.filter((item) => item.id !== id);
+    setNewData(data);
+  };
   return (
     <>
       <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
+        <Button variant="primary" className="my-3" onClick={handleShow}>
+          Add New Blog
         </Button>
 
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Add New Blog</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
+          <Modal.Body>
+            <AddNewBlog handleAddNew={handleAddNew} />
+          </Modal.Body>
         </Modal>
       </>
-
-      <div>
-        <button className="btnAddNew" onClick={handleAddNew}>
-          Add New Blog
-        </button>
-      </div>
       <div className="blogs-container">
         {isLoading === false &&
           newData &&
@@ -58,7 +57,15 @@ const Blog = () => {
           newData.map((item) => {
             return (
               <div className="single-blog" key={item.id}>
-                <div className="title">Title: {item.title}</div>
+                <div className="title">
+                  Title:<span>{item.title}</span>
+                  <span
+                    style={{ color: "blue" }}
+                    onClick={() => deletePost(item.id)}
+                  >
+                    X
+                  </span>
+                </div>
                 <div className="content">{item.body}</div>
                 <button>
                   <Link to={`/blog/${item.id}`}>View details</Link>
